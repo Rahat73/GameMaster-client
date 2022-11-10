5400870ce772e32b57a991390d6f79765ca0ead4
Rahat73 <55106915+Rahat73@users.noreply.github.com>
2022-11-10 23:41:47 +0600

    bg added

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import videoBG from './assets/video_bg.mp4'
 function App() {
   return (
     <div className='App'>
-      {/* <video className='fixed top-0 left-0 w-full h-screen object-cover -z-10' src={videoBG} autoPlay loop muted /> */}
+      <video className='fixed top-0 left-0 w-full h-screen object-cover -z-10' src={videoBG} autoPlay loop muted />
       <RouterProvider router={router}></RouterProvider>
       <ToastContainer
         position="top-right"
